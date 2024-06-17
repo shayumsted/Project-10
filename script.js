31d37188236367da63e1f7638eebaa2f05ceb8c9
Shamira Umstead <sumstead@usf.edu>
2024-06-17 18:29:19 -0400

    git commit -m "code rewrite."

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 // U78784426
-// Defining a function that loads CSV file data
- function loadData() {
-    d3.dsv(',', 'data/purchase_orders.csv').then(function(data) {
-        const ul = d3.select("#purchase-orders");
+document.addEventListener('DOMContentLoaded', function() {
+    d3.dsv(",", "data/purchase_orders.csv", function(d) {
+        return {
+            customerName: d.customerName,
+            orderId: d.orderId,
+            purchaseAmount: d.purchaseAmount};}).then(function(data) {
+        var ul = d3.select("#purchase-orders");
 
-// Dynamic creation of <li> elements for each purchase order
-        ul.selectAll("li")
-            .data(data)
-            .enter()
-            .append("li")
-            .text(function(d) {
-
-// Format text content for each <li> element
-         return `${d.customerName} - Order ID: ${d.orderId} - Purchase Amount: $${d.purchaseAmount}`;
-    });
-}).catch(function(error) {console.error('Error loading the CSV file:', error);
-});}
-
-// Calling loadData function once DOM content is loaded
-document.addEventListener('DOMContentLoaded', loadData);
+        data.forEach(function(d) {
+            ul.append("li")
+              .text(`${d.customerName} - Order ID: ${d.orderId} - Purchase Amount: ${d.purchaseAmount}`);
+        });
+    }).catch(function(error) {console.error('Error loading the CSV file:', error);});
+});
