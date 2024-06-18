cee78aa51bc707cce2dd027c1f03747890457e7e
Shamira Umstead <sumstead@usf.edu>
2024-06-18 11:11:24 -0400

    git commit -m "adjustments."

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ document.addEventListener('DOMContentLoaded', function() {
         return {
             customerName: d.customerName,
             orderId: d.orderId,
-            purchaseAmount: d.purchaseAmount};}).then(function(data) {
+            purchaseAmount: d.purchaseAmount
+        };
+    }).then(function(data) {
         var ul = d3.select("#purchase-orders");
 
         data.forEach(function(d) {
             ul.append("li")
               .text(`${d.customerName} - Order ID: ${d.orderId} - Purchase Amount: ${d.purchaseAmount}`);
         });
-    }).catch(function(error) {console.error('Error loading the CSV file:', error);});
+    }).catch(function(error) {
+        console.error('Error loading the CSV file:', error);
+    });
 });
